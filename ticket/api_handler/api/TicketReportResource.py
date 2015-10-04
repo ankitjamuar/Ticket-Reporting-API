277c32d5f0d4540a1c2b3914feef4bc190d971eb
Ankit Sinha <ankitsinha611@gmail.com>
2015-10-04 18:40:32 +0530

    Till Midnight issue fixed

--- ticket/api_handler/api/TicketReportResource.py
+++ ticket/api_handler/api/TicketReportResource.py
@@ -5,6 +5,7 @@ from tastypie.utils import trailing_slash, dict_strip_unicode_keys
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.db.models import Q, Max
+from datetime import datetime, timedelta
 
 # Models
 from api_handler.models import Ticket, Message
@@ -47,15 +48,30 @@ class TicketReportResource(ModelResource):
     # This API endpoint will return no of closed ticket within specified range
     def ticket_closed(self, request, _from, _to, **kwargs):
         self.method_check(request, allowed=['get'])
-      
+        m = None
+     
         # Get one messages from every Ticket raised whose date is MAX, it will be efficient
         # then getting all the messages that belongs to a EXPERT since ONE EXPERt can answer more that onnce in same ticket
         #qs = User.objects.all()
         #qs = qs.filter(Q(is_staff=True) & Q(is_superuser=False))
 
-        m = Message.objects.values('FK_ticket_id').annotate(Max('updated')).filter(FK_account_id__is_staff=1,updated__range=[_from,_to]).count()
+        # Add 23hours 59 Minutes to reach midnight
+        if _to is not None:
+            _to = datetime.strptime(_to, "%Y-%m-%d") + timedelta(hours=23, minutes=59)
 
+        if _from is None or (_from is None and _to is None):
+            
+            m = Message.objects.values('FK_ticket_id').annotate(Max('updated')).filter(FK_account_id__is_staff=1).count()
 
+        if _to is None and _from is not None:
+            
+            m = Message.objects.values('FK_ticket_id').annotate(Max('updated')).filter(FK_account_id__is_staff=1,updated__gte =_from).count()
+
+        if _from is not None and _to is not None:
+
+            m = Message.objects.values('FK_ticket_id').annotate(Max('updated')).filter(FK_account_id__is_staff=1,updated__range=[_from,_to]).count()
+
+     
         return self.create_response(request, m)
 
 
